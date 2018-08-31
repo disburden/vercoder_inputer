ae61086403e80f9fc284c9d9ce9b1727792f4a54
disburden <disburden.gamil.com>
2018-09-01 01:30:30 +0800

    ready for pub

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -4,86 +4,64 @@ import 'package:vercoder_inputer/vercoder_inputer.dart';
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-	
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: new MyHomePage(title: 'vercode inputer'),
-    );
-  }
+
+	// This widget is the root of your application.
+	@override
+	Widget build(BuildContext context) {
+		return new MaterialApp(
+			title: 'Flutter Demo',
+			theme: new ThemeData(
+				// This is the theme of your application.
+				//
+				// Try running your application with "flutter run". You'll see the
+				// application has a blue toolbar. Then, without quitting the app, try
+				// changing the primarySwatch below to Colors.green and then invoke
+				// "hot reload" (press "r" in the console where you ran "flutter run",
+				// or press Run > Flutter Hot Reload in IntelliJ). Notice that the
+				// counter didn't reset back to zero; the application is not restarted.
+				primarySwatch: Colors.blue,
+			),
+			home: new MyHomePage(title: 'vercode inputer'),
+		);
+	}
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+	MyHomePage({
+		Key key,
+		this.title
+	}): super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+	// This widget is the home page of your application. It is stateful, meaning
+	// that it has a State object (defined below) that contains fields that affect
+	// how it looks.
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+	// This class is the configuration for the state. It holds the values (in this
+	// case the title) provided by the parent (in this case the App widget) and
+	// used by the build method of the State. Fields in a Widget subclass are
+	// always marked "final".
 
-  final String title;
+	final String title;
 
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
+	@override
+	_MyHomePageState createState() => new _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-
-
+class _MyHomePageState extends State < MyHomePage > {
 	WGQVerCodeInputer verCodeInputer = WGQVerCodeInputer(codeLength: 6, parnetSize: Size(375.0, 48.0), finishInput: (verCode, ctx) {
 		print("veris $verCode");
-		showDialog(
-			context: ctx,
-			builder: (_) {
-				return Center(
-					child: Material(
-						child:Text(verCode),
-					)
-				);
-			},
-		).then((e) {
-			if (e != null) {}
-		});
 	}, );
 
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return new Scaffold(
-      appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
-      ),
-      body: Padding(
-		  padding:EdgeInsets.only(top:100.0),
-		  child: verCodeInputer,
-	  )
-   
-// This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+	@override
+	Widget build(BuildContext context) {
+		return new Scaffold(
+			appBar: new AppBar(
+				title: new Text(widget.title),
+			),
+			body: Padding(
+				padding: EdgeInsets.only(top: 100.0),
+				child: verCodeInputer,
+			)
+		);
+	}
 }
--- lib/vercoder_inputer.dart
+++ lib/vercoder_inputer.dart
@@ -2,10 +2,3 @@
 library WGQVerCodeInputer;
 
 export 'src/WGQVerCodeInputer.dart';
-// library vercoder_inputer;
-
-// /// A Calculator.
-// class Calculator {
-//   /// Returns [value] plus 1.
-//   int addOne(int value) => value + 1;
-// }
